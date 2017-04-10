a4e2b1380c2a414797986e1def21cb068356b47b
lancasp22 <lancaster_peter@hotmail.com>
2017-04-10 14:42:18 +0100

    Update README.md Add more tests

--- README.md
+++ README.md
@@ -2,6 +2,24 @@
 
 Elixir simple word similarity check - version 1
 
+Possible use as a technical test:
+A surname consists of a single string of upper case LATIN alphabetical characters.
+
+Write a function to evaluate an input string for similarity against a given surname that returns a score depending on the similarity.
+If the strings are the same return 100 otherwise if they are not anagrams of each other then return 0. If they are anagrams of each other then calculate the minimum of adjacent swaps in the string that are required to make the strings the same and return a score of 100 reduced by 5 for each swap required, but with a minimum score of 0.
+
+Examples:
+("ABCDEF", "ABCDEF") returns 100
+("ABDCEF", "BBCDEF") returns 0
+("BBDCEF", "BBCDEF") returns 95
+("HARRIS", "HARIRS") returns 95
+("HARRIS", "HRARIS") returns 95
+("BACDFE", "ABCDEF") returns 90
+("ABCFDE", "ABCDEF") returns 90
+("ANNE", "ENNA") returns 75
+("FEDCBA", "ABCDEF") returns 25
+("GFEDCBA", "ABCDEFG") returns 0
+
 ## Installation
 
 If [available in Hex](https://hex.pm/docs/publish), the package can be installed as:
--- test/name_scorer_test.exs
+++ test/name_scorer_test.exs
@@ -6,8 +6,17 @@ defmodule NameScorerTest do
     assert NameScorer.score("ABCDEF", "ABCDEF") == 100
   end
 
+  test "Not an anagram" do
+    assert NameScorer.score("ABDCEF", "BBCDEF") == 0
+  end
+
   test "1 swap required returns 95" do
     assert NameScorer.score("BBDCEF", "BBCDEF") == 95
+  end
+
+  test "swap of ambiguous letter returns 95" do
+    assert NameScorer.score("HARRIS", "HARIRS") == 95
+    assert NameScorer.score("HARRIS", "HRARIS") == 95
   end
 
   test "2 simple swaps required returns 90" do
@@ -18,16 +27,17 @@ defmodule NameScorerTest do
     assert NameScorer.score("ABCFDE", "ABCDEF") == 90
   end
 
-  test "6 characters reversed returns 25" do
+  test "swap of ANNE to ENNA returns 75" do
+    assert NameScorer.score("ANNE", "ENNA") == 75
+  end
+
+  test "6 unique characters reversed returns 25" do
     assert NameScorer.score("FEDCBA", "ABCDEF") == 25
   end
 
-  test "7 characters reversed returns 0" do
+  test "7 unique characters reversed returns 0" do
     assert NameScorer.score("GFEDCBA", "ABCDEFG") == 0
   end
 
-  test "Not an anagram" do
-    assert NameScorer.score("ABDCEF", "BBCDEF") == 0
-  end
 
 end
